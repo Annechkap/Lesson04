9032c2eff7d0b854ec3a88b41a77b9f732642d8a
Anna <annechkap@list.ru>
2022-10-16 19:15:48 +0300

    Добавлена задача 001

---
+++ 001/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1.  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B (и в нулевую степень). 
+//Использовать свои функции, не использовать Math.Pow
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int A = Prompt($"Введите число A   ");
+int B = Prompt($"Введите число B   ");
+
+int result = 1;
+for (int i = 1; i <= B; i++)
+{
+    result = result * A;
+}
+
+Console.WriteLine($"{result}");
