c37649ccde3bdf6708dcd14e35bca29951f1b340
Anna <annechkap@list.ru>
2022-10-16 21:04:25 +0300

    Добавлена задача 003

---
+++ 003/Program.cs
@@ -0,0 +1,22 @@
+﻿// 3. Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
+
+int[] Created_Array()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0 , 10);
+    }
+    return array;
+}
+
+void Print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+}
+
+int[] arr = Created_Array();
+Print(arr);
